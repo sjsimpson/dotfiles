c8b4df87cd682290c5f94184d7df12d04627b30a
Spencer Simpson <ssimpson@qualtrics.com>
2020-12-30 10:21:28 -0700

    v1.0.2

--- exa/install.fish
+++ exa/install.fish
@@ -1,9 +1,8 @@
 #!/usr/bin/env fish
 if command -qa exa
-	abbr -a ls 'exa'
+	abbr -a ls 'exa -aFG'
 	abbr -a lg 'exa --git'
 	abbr -a l 'exa -lah'
-	abbr -a la 'exa -a'
 	abbr -a ll 'exa -l'
 	abbr -a lt 'exa -lT'
 else
